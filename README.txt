9a05a87e77f2916278440345b742066aa77ed960
Gurpartap Singh <gurpartap_singh@41470.no-reply.drupal.org>
2009-01-16 18:20:02 +0000

    #330057 - Updated page names are not being saved Corrected the installations instructions in README.txt

--- README.txt
+++ README.txt
@@ -1,31 +1,36 @@
 About
 _____
-Break long pages into smaller ones automatically by words(or characters) limit, or by means of a customizable pagination tag. For example:
 
-first page here.
+Break long pages into smaller ones automatically by words/characters limit, or by means of a customizable pagination tag. For example:
+
+First page here.
 <!--pagebreak-->
-second page here.
+Second page here.
+<!--pagebreak-->
+More pages here.
+
 
 Install
 _____
-1. Copy the paging-6.x-x.x.tar.gz contents to sites/SITENAME/modules directory (usually sites/all/modules).
 
-2. Enable paging for the content types you want to use it on, in the paging configuration at http://www.example.com/drupal/?q=admin/settings/paging.
+1. Copy the paging-6.x-x.x.tar.gz contents to sites/SITENAME/modules directory (usually sites/all/modules) and enable the module at admin/build/modules path.
 
-3. Then enable the paging filter for the relevant input format(usually Filtered HTML), in Input Formats configuration at http://www.example.com/drupal/?q=admin/settings/filters.
+2. Enable paging for the content types you want to use it on, in the paging configuration at admin/settings/paging path.
+
+3. Then enable the paging filter for the relevant input format(usually Filtered HTML), in Input Formats configuration at admin/settings/filters path.
 
 4. Your module is now setup and ready to be used.
-
-Configuration
-_____
 
 
 Support
 -----
+
 If you experience a problem with paging module, file a request or issue on the paging queue at http://drupal.org/project/issues/paging. DO NOT POST IN THE FORUMS. Posting in the issue queues is a direct line of communication with the module authors.
+
 
 Credits
 _____
+
 Original module written by Marco Scutari.
 Rewritten and considerably shortened and made more Drupal-friendly by Earl Miles.
-Yet again rewritten, extended and maintained by Gurpartap Singh.
+Yet again rewritten, extended and currently maintained by Gurpartap Singh.
--- paging.js
+++ paging.js
@@ -1,38 +1,36 @@
 // $Id$
 
-var names = new Array();
+function paging_handle_names(pages) {
+  var output = '';
 
-Drupal.behaviors.paging = function(context) {
-  $('textarea.teaser').each(function() {
-    var include = $('#' + this.id.substring(0, this.id.length - 2) + 'include');
-    var widget = Drupal.settings.paging.widget;
-
-    if (widget == 1) {
-    // Add paging separator string image.
-      var path = Drupal.settings.basePath + Drupal.settings.paging.module_path;
-      var string = Drupal.t('Insert page separator');
-      var image = $('<div class="paging-button-wrapper"><img alt="" src="' + path + '/paging-separator.png" class="paging-separator-image" /></div>');
-      $(include).parent().parent().before(image);
-      $('img', image).attr('alt', string).attr('title', string).bind('click', paging_insert_separator);
+  if (pages.length > 0) {
+    for (var x = 0; x < pages.length; x++) {
+      var title = $('#edit-title').val();
+      var name = Drupal.t('!title - Page !number', {'!title': title, '!number': (x + 1)});
+      pages[x] = (pages[x] || name);
+      output += '<label for="edit-paging-title[' + x + ']">' + Drupal.t("Name of !number page: ", {'!number': (x + 1).ordinal()}) + '</label>'
+        + "\t" + '<input type="text" class="form-text" value="' + pages[x] + '" size="20" name="edit-paging-title[' + x + ']" maxlength="255"/>' + "\n";
     }
-    else if (widget == 2) {
-      // Add paging separator string button.
-      var button = $('<div class="paging-button-wrapper"><input type="button" class="paging-separator-button" /></div>');
-      $(include).parent().parent().before(button);
-      $('input', button).val(Drupal.t('Insert page separator')).bind('click', paging_insert_separator);
-    }
-  });
-
-  var page_names = Drupal.settings.paging.page_names;
-  if (page_names == 1) {
-    $('#edit-body-wrapper').after('<div id="paging-names-wrapper"><fieldset class="collapsible form-item" id="paging-page-names" style="overflow: hidden;"><legend class="">' + Drupal.t("Page names") + '</legend><div id="paging-names-inner" class="form-item"></div></fieldset></div>');
-    $('textarea#edit-body').each(paging_handle_names).bind('click keyup blur', paging_handle_names);
-    $('#node-form').submit(function() {
-      $('textarea#edit-body').each(function() {
-        $(this).val('<!--pagenames:' + paging_return_names().join('||') + "-->" + this.value);
-      });
+    $('#paging-names-wrapper').show().each(function() {
+      $(this).html('<fieldset class="form-item" id="paging-page-names" style="overflow: hidden;">'
+      + '<legend class="">' + Drupal.t("Page names") + '</legend><div id="paging-names-inner" class="form-item"></div></fieldset>');
     });
+    $('#paging-names-wrapper #paging-names-inner').html(output);
   }
+  else {
+    $('#paging-names-wrapper').hide();
+  }
+}
+
+function paging_return_names() {
+  var names = new Array();
+  var i = 0;
+  $('#paging-page-names').find('input[@type=text]').each(function() {
+    names[i] = $(this).val();
+    i++;
+  }).parents('fieldset').remove();
+
+  return names;
 }
 
 function paging_insert_separator() {
@@ -56,48 +54,66 @@ function paging_insert_separator() {
   return false;
 }
 
-function paging_handle_names(event) {
-  var output = '';
-  var separator = Drupal.settings.paging.separator;
-  var str = $(this).val();
-
-  var pages = str.indexOf(separator) != -1 ? str.split(separator) : 0;
-
-  var match = str.match(/<!--pagenames:(.*?)-->/) || [];
-  $(this).val(str.replace(/<!--pagenames:(.*?)-->/, ''));
-
-  // TODO: names var doesn't store updated names.
-  names = names || (match[1] ? match[1].split('||') : []);
-
-  if (pages.length > 0) {
-    for (var x = 0; x < pages.length; x++) {
-      var title = $('#edit-title').val();
-      names[x] = (names[x] || Drupal.t('!title - Page !number', {'!title': title, '!number': (x + 1)}));
-      output += '<label for="edit-paging-title[' + x + ']">' + Drupal.t("Name of !number page: ", {'!number': (x + 1).ordinal()}) + '</label>' + "\t" + '<input type="text" class="form-text" value="' + names[x] + '" size="20" name="edit-paging-title[' + x + ']" maxlength="255"/>' + "\n";
-    }
-    $('#paging-names-wrapper').show();
-    $('#paging-names-wrapper #paging-names-inner').html(output);
-  }
-  else {
-    $('#paging-names-wrapper').hide();
-  }
-}
-
-function paging_return_names() {
-  var names = new Array();
-  var i = 0;
-  $('#paging-page-names').find('input[@type=text]').each(function() {
-      names[i] = $(this).val();
-      i++;
-    }).parents('fieldset').remove();
-
-  return names;
-}
-
 Number.prototype.ordinal = function () {
   return this + (
     (this % 10 == 1 && this % 100 != 11) ? '<sup>st</sup>' :
     (this % 10 == 2 && this % 100 != 12) ? '<sup>nd</sup>' :
     (this % 10 == 3 && this % 100 != 13) ? '<sup>rd</sup>' : '<sup>th</sup>'
   );
+}
+
+Drupal.behaviors.paging = function(context) {
+  // Handle the widget button to insert paging separator.
+  $('textarea.teaser').each(function() {
+    var include = $('#' + this.id.substring(0, this.id.length - 2) + 'include');
+    var widget = Drupal.settings.paging.widget;
+
+    if (widget == 1) {
+    // Add paging separator string image.
+      var path = Drupal.settings.basePath + Drupal.settings.paging.module_path;
+      var string = Drupal.t('Insert page separator');
+      var image = $('<div class="paging-button-wrapper"><img alt="" src="' + path + '/paging-separator.png" class="paging-separator-image" /></div>');
+      $(include).parent().parent().before(image);
+      $('img', image).attr('alt', string).attr('title', string).bind('click', paging_insert_separator);
+    }
+    else if (widget == 2) {
+      // Add paging separator string button.
+      var button = $('<div class="paging-button-wrapper"><input type="button" class="paging-separator-button" /></div>');
+      $(include).parent().parent().before(button);
+      $('input', button).val(Drupal.t('Insert page separator')).bind('click', paging_insert_separator);
+    }
+  });
+
+  var page_names = Drupal.settings.paging.page_names;
+  if (page_names == 1) {
+  // Page names interface is enabled.
+    $('#edit-body-wrapper').after('<div id="paging-names-wrapper"></div>');
+
+    $('textarea#edit-body').each(function() {
+      var str = $(this).val();
+      var match = str.match(/<!--pagenames:(.*?)-->/) || [];
+      $(this).val(str.replace(/<!--pagenames:(.*?)-->/, ''));
+      var pages = typeof match[1] != 'undefined' ? match[1].split('||') : [];
+      paging_handle_names(pages);
+    }).bind('click keyup blur', function() {
+      var str = $(this).val();
+      var separator = Drupal.settings.paging.separator;
+      var count = 0;
+      var current_names = paging_return_names();
+      var pages = new Array((current_names[count] ? current_names[count] : ''));
+      var pos = str.indexOf(separator);
+      while (pos != -1) {
+        count++;
+        pages.push(current_names[count] ? current_names[count] : '');
+        pos = str.indexOf(separator, pos + 1);
+      }
+      paging_handle_names(str.indexOf(separator) != -1 ? pages : 0);
+    });
+
+    $('#node-form').submit(function() {
+      $('textarea#edit-body').each(function() {
+        $(this).val('<!--pagenames:' + paging_return_names().join('||') + "-->" + this.value);
+      });
+    });
+  }
 }
